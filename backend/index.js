d6173a05ae98634a7e4392ba425169a7f12b389d
Almer Farand Rafael <farandalmer4@gmail.com>
2025-05-27 14:15:37 +0700

    Update port dalam docker dab index.js

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,12 @@ import keuanganRoutes from "./routes/Keuanganroute.js";
 import userRoutes from "./routes/UserRoute.js";
 
 const app = express();
-app.use(express.json());
+const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/keuangan", keuanganRoutes);
 app.use("/api/users", userRoutes);
 
-const server = app.listen(5000, '0.0.0.0', () => {
-  const addr = server.address();
-  console.log(`Server running on http://0.0.0.0:${addr.port}`);
-  console.log(`Server details:`, {
-    port: addr.port,
-    address: addr.address,
-    family: addr.family
-  });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
